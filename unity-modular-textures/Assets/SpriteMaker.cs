c954c25f8dbce328f7b66d4ef1576f7e647e586b
Jay Koutavas <jay@heynow.com>
2022-05-19 08:17:28 -0400

    Completed tutorial part 4

--- unity-modular-textures/Assets/SpriteMaker.cs
+++ unity-modular-textures/Assets/SpriteMaker.cs
@@ -4,40 +4,59 @@ using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteMaker : MonoBehaviour {
-    public Texture2D src;
+    public Texture2D[] layers;
     SpriteRenderer rend;
 
-    // Start is called before the first frame update
+    Texture2D tex;
+
+
     void Start() {
+        //making a texture
+        makeTexture();
+
+        //making a sprite from that texture
+        makeSprite();
+    }
+
+    void makeTexture() {
+        //create a texture
         rend = GetComponent<SpriteRenderer>();
 
-        // create a texture
-        Texture2D tex = new Texture2D(src.width, src.height);
+        //create a texture
+        tex = new Texture2D(layers[0].width, layers[0].height);
         Color[] colorArray = new Color[tex.width * tex.height];
 
-        Color[] srcArray = src.GetPixels();
+        Color[][] srcArray = new Color[layers.Length][];
+
+        //populate source array with layer arrays
+        for (int i = 0; i < layers.Length; i++) {
+            srcArray[i] = layers[i].GetPixels();
+        }
 
         for (int x = 0; x < tex.width; x++) {
             for (int y = 0; y < tex.height; y++) {
                 int pixelIndex = x + y * tex.width;
-                Color srcPixel = srcArray[pixelIndex];
-                colorArray[pixelIndex] = srcPixel;
+                for (int i = 0; i < layers.Length; i++) {
+                    Color srcPixel = srcArray[i][pixelIndex];
+                    if (srcPixel.a > 0) {
+                        colorArray[pixelIndex] = srcPixel;
+                    }
+                }
             }
         }
         tex.SetPixels(colorArray);
         tex.Apply();
 
-        // create a sprite from that texture
-        Sprite newSprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.one * 0.5f);
-
-        // assign our procedural sprite to rend sprite
-        rend.sprite = newSprite;
+        //confirm any settings
         tex.wrapMode = TextureWrapMode.Clamp;
         tex.filterMode = FilterMode.Point;
     }
 
-    // Update is called once per frame
-    void Update() {
+    void makeSprite() {
+        //create a sprite from that texture
+        Sprite newSprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.one * 0.5f);
 
+        //assign our procedural sprite to rend sprite
+        rend.sprite = newSprite;
     }
 }
