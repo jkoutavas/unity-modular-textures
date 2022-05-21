48753260590d4f44faec0cb7b8db77c80fa0bfc8
Jay Koutavas <jay@heynow.com>
2022-05-20 21:40:50 -0400

    Completed tutorial part 6

--- unity-modular-textures/Assets/SpriteMaker.cs
+++ unity-modular-textures/Assets/SpriteMaker.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteMaker : MonoBehaviour {
-    public Texture2D[] layers;
+
     SpriteRenderer rend;
+
+    public Texture2D[] layers;
+    public Color[] layerColors;
 
     Texture2D tex;
 
 
+    // Use this for initialization
     void Start() {
+        rend = GetComponent<SpriteRenderer>();
+
         //making a texture
         makeTexture();
 
@@ -20,12 +26,12 @@ public class SpriteMaker : MonoBehaviour {
 
     void makeTexture() {
         //create a texture
-        rend = GetComponent<SpriteRenderer>();
-
-        //create a texture
         tex = new Texture2D(layers[0].width, layers[0].height);
+
+        //array to store the destination texture's pixels
         Color[] colorArray = new Color[tex.width * tex.height];
 
+        //array of colors derived from the source Texture
         Color[][] srcArray = new Color[layers.Length][];
 
         //populate source array with layer arrays
@@ -33,11 +39,19 @@ public class SpriteMaker : MonoBehaviour {
             srcArray[i] = layers[i].GetPixels();
         }
 
+        // iterate through each pixel, copying the source index to the destination index
         for (int x = 0; x < tex.width; x++) {
             for (int y = 0; y < tex.height; y++) {
                 int pixelIndex = x + y * tex.width;
                 for (int i = 0; i < layers.Length; i++) {
                     Color srcPixel = srcArray[i][pixelIndex];
+
+                    //APPLY LAYER COLOR IF NECESSARY
+                    if (srcPixel.r != 0 && srcPixel.a != 0) {
+                        srcPixel = applyColorToPixel(srcPixel, layerColors[i]);
+                    }
+
+                    //NORMAL BLENDING BASED ON ALPHA
                     if (srcPixel.a == 1) {
                         colorArray[pixelIndex] = srcPixel;
                     } else if (srcPixel.a > 0) {
@@ -46,12 +60,14 @@ public class SpriteMaker : MonoBehaviour {
                 }
             }
         }
+
+        //transfer the array to the texture and apply the pixels
         tex.SetPixels(colorArray);
         tex.Apply();
 
         //confirm any settings
         tex.wrapMode = TextureWrapMode.Clamp;
-        tex.filterMode = FilterMode.Point;
+        //    tex.filterMode = FilterMode.Point;
     }
 
     void makeSprite() {
@@ -68,5 +84,12 @@ public class SpriteMaker : MonoBehaviour {
         Color destLayer = dest * destAlpha;
         Color srcLayer = src * srcAlpha;
         return destLayer + srcLayer;
+    }
+
+    Color applyColorToPixel(Color pixel, Color applyColor) {
+        if (pixel.r == 1f) {
+            return applyColor;
+        }
+        return pixel * applyColor;
     }
 }
